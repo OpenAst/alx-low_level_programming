6fd6089358119eb492585232929d491675ec7f28
OpenAst <isrealme0@gmail.com>
2022-12-09 03:03:34 -0800

    sizeof() operator in C

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main-Print the sizeof C data types
+ *
+ * Return: always (0)
+ *
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %lu bytes(s)", (unsigned long)sizeof(a);
+	printf("Size of a char: %lu bytes(s)", (unsigned long)sizeof(b);
+	printf("Size of a char: %lu bytes(s)", (unsigned long)sizeof(c);
+	printf("Size of a char: %lu bytes(s)", (unsigned long)sizeof(d);
+	printf("Size of a char: %lu bytes(s)", (unsigned long)sizeof(f);
+}
