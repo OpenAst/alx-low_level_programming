7edebdade7660a924ca3749a751214b7a948745a
OpenAst <isrealme0@gmail.com>
2022-12-09 08:56:46 -0800

    alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main- print out lowercase alphabet
+ *
+ * Return: (0)
+ *
+ */
+
+int main(void)
+{
+	char alpha;
+
+	for (alpha = 'a'; alpha <= 'z'; alpha++)
+		putchar(alpha);
+
+	putchar('\n');
+
+	return (0);
+}
