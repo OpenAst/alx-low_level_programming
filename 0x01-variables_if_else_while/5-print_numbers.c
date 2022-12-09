1765a0b835a3ccccac80ec66fd14c8c67f4c63d5
OpenAst <isrealme0@gmail.com>
2022-12-09 10:04:32 -0800

    print numers in base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main- prints all single digits in base 10
+ *
+ * Return: 0
+ *
+ */
+
+int main(void)
+{
+	int digit;
+
+	for (digit = 0; digit < 10; digit++)
+		printf("%d\n", digit);
+
+	return (0);
+}
