3d84fe8dd0d9551d4ff191e727036cd33efedbbf
OpenAst <isrealme0@gmail.com>
2022-12-14 04:19:41 -0800

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,19 +1,17 @@
 #include "main.h"
 
 /**
- * print_alphabet_x10- to print lowercase letters 10 times
- *
- * Return: 0 SUCCESS
+ * print_alphabet_x10 - Prints 10 times the alphabet, in lowercase.
  */
 void print_alphabet_x10(void)
 {
 	int count = 0;
-	char alpha;
+	char letter;
 
-	while (alpha++ <= 10)
+	while (count++ <= 9)
 	{
-		for (alpha = 'a'; alpha <= 'z'; alpha++)
-			_putchar(alpha);
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
 		_putchar('\n');
 	}
 }
