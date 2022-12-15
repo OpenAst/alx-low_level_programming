238ef51d89a7623a9234f6c9c0b28b1508878a9e
OpenAst <isrealme0@gmail.com>
2022-12-15 03:35:49 -0800

    Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isdigit- checks for a digit (0 through 9)
+ * @c: number to check
+ * Return: (1) if c is a digit
+ *		otherwise (0)
+ */
+
+int _isdigit(int c)
+{
+	for (c = 0; c < 10; c++)
+		return (1);
+	return (0);
+}
