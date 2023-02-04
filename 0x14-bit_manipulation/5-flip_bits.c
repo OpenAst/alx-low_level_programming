323a2027a5c1a0d59bc88c2d1268edec94914b52
OpenAst <isrealme0@gmail.com>
2023-02-03 17:16:05 -0800

    Task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -7,15 +7,15 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int differnce, result;
+	unsigned long int difference, result;
 	unsigned int j, i;
 
 	j = 0;
 	result = 1;
-	differnce = n ^ m;
+	difference = n ^ m;
 	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
 	{
-		if (result == (differnce & result))
+		if (result == (difference & result))
 			j++;
 		result <<= 1;
 	}
